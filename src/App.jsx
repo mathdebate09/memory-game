42db0674e788402e4cedb6e1e27535a70e502884
Jay Singh <learn.jayssingh@gmail.com>
2024-06-18 23:39:29 +0530

    Styles for header & score-card sections

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,6 @@ import Footer from './components/Footer'
 import data from './utils/data'
 import shuffle from './utils/shuffleArray.js'
 
-import logoImg1 from './assets/images/icons/mm-logo-1.png'
-import logoImg2 from './assets/images/icons/mm-logo-2.png'
-
 function App() {
   const [currentScore, setCurrentScore] = useState(0)
   const [bestScore, setBestScore] = useState(0)
@@ -18,8 +15,6 @@ function App() {
   const cardData = shuffle(data)
 
   function handleCardClick(id) {
-    console.log(id)
-    console.log(data.filter(d => d.id === id))
     if (selectedCards.includes(id)) {
       reset()
       return;
@@ -28,7 +23,6 @@ function App() {
     setCurrentScore(currentScore => currentScore + 1)
     currentScore === bestScore ? setBestScore(bestScore => bestScore + 1) : null
     setSelectedCards(currentCards => [...currentCards, id])
-    console.log(selectedCards)
   }
 
   function reset() {
@@ -49,9 +43,9 @@ function App() {
 
   return (
     <div className="container">
-      <header><img src={logoImg1}></img><img src={logoImg2}></img></header>
+      <header><p>MINECRaFT</p><p>MEMORY</p></header>
       <section className="score-div">
-        <p>Score: {currentScore} | Best: {bestScore}</p>
+        <p>Score- {currentScore} -- Best- {bestScore}</p>
       </section>
       <section className="cards-tray">
         {cardArray}
